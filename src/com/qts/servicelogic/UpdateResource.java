3ee7ba44a86e8d25700043bebd8c03b629042d51
Anup Narayan Roy <anupnarayanroy@outlook.com>
2018-06-27 13:55:10 +0530

    changes for teaching staff

--- src/com/qts/servicelogic/UpdateResource.java
+++ src/com/qts/servicelogic/UpdateResource.java
@@ -2,7 +2,6 @@ package com.qts.servicelogic;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -13,14 +12,16 @@ import com.qts.model.Resource;
 public class UpdateResource {
 	static Connection connection = null;
 	static Statement statement = null;
+	//String user = "STUDENT";
+	static String user = "TEACHING STAFF";
 	public static void main(String [] args) throws SQLException {
 		List<Resource> resourceList = new ArrayList<Resource>();
 		try {
 			connection = new DBConnection().getConnection();
 			if(null != connection) {
 				statement = connection.createStatement();
-				String selectQueryForUserId = "SELECT user_id,mobile_no FROM resource WHERE resource_type = (SELECT rec_id FROM resource_type WHERE resource_type_name = 'STUDENT' AND is_active = true) AND is_active = true";
-				ResultSet result = statement.executeQuery(selectQueryForUserId);
+				String selectQueryForStudent = "SELECT user_id,mobile_no FROM resource WHERE resource_type = (SELECT rec_id FROM resource_type WHERE resource_type_name = '"+ user +"' AND is_active = true) AND is_active = true";
+				ResultSet result = statement.executeQuery(selectQueryForStudent);
 				while(result.next()) {
 					Resource resource = new Resource();
 					if(null!=result.getString("user_id")) resource.setUserId(result.getString("user_id"));
@@ -28,7 +29,7 @@ public class UpdateResource {
 					resourceList.add(resource);
 				}
 				System.out.println(resourceList.size());
-				String status = updateMobileOfResource(resourceList);
+				String status = updateMobileOfResource(resourceList, user);
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -38,7 +39,7 @@ public class UpdateResource {
 		}
 	}
 	
-	public static String updateMobileOfResource(List<Resource> resourceList) throws SQLException {
+	public static String updateMobileOfResource(List<Resource> resourceList, String user) throws SQLException {
 		String status = "fail";
 		try {
 			for(Resource r : resourceList) {
@@ -46,10 +47,22 @@ public class UpdateResource {
 				System.out.print("userId:"+userId);
 				String mobile = r.getMobile();
 				System.out.println("Mobile:"+mobile);
-				if(mobile.contains("(") || mobile.contains("-")) {
-					mobile = mobile.substring(5, 16);
-					mobile = mobile.replace("-", "");
+				if(user.equalsIgnoreCase("STUDENT")) {
+					if(mobile.contains("(") || mobile.contains("-")) {
+						mobile = mobile.substring(5, 16);
+						mobile = mobile.replace("-", "");
+					}
+				}else if(user.equalsIgnoreCase("TEACHING STAFF")) {
+					if(mobile.contains("(")) {
+						mobile = mobile.substring(5, 16);
+						mobile = mobile.replace("-", "");
+					}else if(mobile.contains("-")) {
+						mobile = mobile.replace("-", "");
+					}else if(mobile.contains(" ")) {
+						mobile = mobile.replace(" ", "");
+					}
 				}
+				
 				String updateQueryForMobile = "UPDATE resource SET mobile_no = '"+ mobile +"' WHERE user_id = '"+userId+"' AND is_active = true";
 				statement.executeUpdate(updateQueryForMobile);
 			}
